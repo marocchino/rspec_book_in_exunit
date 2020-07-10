eed37586156ec020b8bf97c63a0ed073dc32598a
marocchino <marocchino@users.noreply.github.com>
2020-07-10 15:01:57 +0900

    test: sandwich is delicious

--- test/rspec_book_test.exs
+++ test/rspec_book_test.exs
@@ -1,8 +1,14 @@
 defmodule RspecBookTest do
   use ExUnit.Case
   doctest RspecBook
+  alias RspecBook.Sandwich
 
-  test "greets the world" do
-    assert RspecBook.hello() == :world
+  describe "an ideal sandwich" do
+    test "is delicious" do
+      sandwich = %Sandwich{taste: 'delicious'}
+      assert taste == 'delicious'
+    end
   end
+
+  def build(:sandwich), do: %{}
 end
